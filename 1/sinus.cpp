76f98b171f440a1f2880fa2f7f251daa6708c77c
God <arturbakirov1730@gmail.com>
2026-02-21 20:23:02 +0700

    add 1/sinus, add gitignore

---
+++ 1/sinus.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <cmath>
+#include <numbers>
+
+#define PI 3.14159265358979323846
+
+#ifdef USE_FLOAT
+    using Type = float;
+#else
+    using Type = double;
+#endif
+
+
+int main(){
+    int N = 10000000;
+    std::vector<Type> Arr(N);
+    
+    if(typeid(Type)==typeid(float)){
+        for(int i=0; i<N; i++){
+            Arr[i] = std::sinf(2*PI*i/N);
+        }
+    }
+    else{
+        for(int i=0; i<N; i++){
+            Arr[i] = std::sin(2*PI*i/N);
+        }
+    }
+
+    float sum;
+
+    for(int i=0; i<N; i++){
+        sum+=Arr[i];
+    }
+
+    std::cout << sum << "\n";
+}
